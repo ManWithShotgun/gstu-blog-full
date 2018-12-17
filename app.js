9d341103ac1d7bce35e006cadf6d5a25f12eee38
Unknown <u3n3ter7@mail.ru>
2018-12-17 19:06:46 +0300

    fix

--- app.js
+++ app.js
@@ -12,28 +12,28 @@ var api = require('./routes/api');
 var app = express();
 
 mongoose.Promise = require('bluebird');
-mongoose.connect(config.database, { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection succesful'))
+mongoose.connect(config.database, {promiseLibrary: require('bluebird')})
+  .then(() => console.log('connection succesful'))
   .catch((err) => console.error(err));
 
 app.use(passport.initialize());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(bodyParser.urlencoded({'extended': 'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/blogs', express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
